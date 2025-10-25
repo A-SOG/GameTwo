a15704f94de7c8f6e1e5ce7e18cb41dff7df7f95
Anderson <2914725163@qq.com>
2025-10-25 22:27:25 +0800

    111

--- src/world/effect.h
+++ src/world/effect.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "../core/object_world.h"
-#include "../affiliate/sprite_anim.h"
+#include "../affilicate/sprite_anim.h"
 #include <string>
 
 class Effect : public ObjectWorld
@@ -12,7 +12,6 @@ public:
     static Effect* addEffectChild(Object* parent, const std::string& file_path, glm::vec2 pos, float scale = 1.0f, ObjectWorld* next_object = nullptr);
     virtual void update(float dt) override;
 
-    // setters and getters
     void setSpriteAnim(SpriteAnim* sprite) { sprite_ = sprite; }
     SpriteAnim* getSpriteAnim() { return sprite_; }
     void setNextObject(ObjectWorld* next_object) { next_object_ = next_object; }
